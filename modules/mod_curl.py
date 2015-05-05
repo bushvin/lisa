f43f3aa629492dc3e5f4a698517641ec9768a859
William Leemans <bushvin@users.noreply.github.com>
2015-05-05 10:50:11 +0200

    Add support for CSV files
    Introduce support for CSV files

--- modules/mod_curl.py
+++ modules/mod_curl.py
@@ -7,13 +7,10 @@
 # license: GPL v3
 
 import pycurl
+import sys
 from io import BytesIO
 import json
-#from StringIO import StringIO
-
-#import sys
-#import re
-#from jinja2 import Template
+import csv
 
 class mod_curl():
     _hostvars = dict()
@@ -28,11 +25,6 @@ class mod_curl():
         self._org_hostvars = hostvars
   
     def getHostvars(self):
-        try:
-            self._args["json"]
-        except:
-            self._args["format"] = "json"
-
         buffer = BytesIO()
         con = pycurl.Curl()
         con.setopt(con.URL, self._args["url"])
@@ -40,13 +32,96 @@ class mod_curl():
         con.perform()
         rc = con.getinfo(con.RESPONSE_CODE)
         con.close()
-        self._hostvars = self._convert(buffer.getvalue(), self._args["format"])
-
+        self._convert(buffer.getvalue())
+        self._add_prefix()
         return self._hostvars
 
-    def _convert(self, value, format):
-        if format == "json":
-            return json.loads(value)
+    def _add_prefix(self):
+        try:
+            self._args["prefix"]
+        except:
+            self._args["prefix"] = ""
+
+        new_hostvars = dict()
+        for hostname in self._hostvars:
+            nf = dict()
+            for fact in self._hostvars[hostname]:
+                nf.update({self._args["prefix"] + fact: self._hostvars[hostname][fact]})
+            new_hostvars.update({hostname: nf})
+        self._hostvars = new_hostvars
+            
+
+    def _convert(self, value):
+        try:
+            self._args["format"]
+        except:
+            self._args["format"] = "json"
+
+        if self._args["format"] == "json":
+            self._hostvars = json.loads(value)
+
+        elif self._args["format"] == "csv":
+            try:
+                self._args['header']
+            except:
+                self._args['header'] = ""
+
+            try:
+                self._args['exclude']
+            except:
+                self._args['exclude'] = ""
+
+            try:
+                self._args['delimiter']
+            except:
+                self._args['delimiter']=","
+
+            try:
+                self._args['quotechar']
+            except:
+                self._args['quotechar']=""
+
+            header = []
+            exclude = []
+
+            if self._args['header'] != "":
+                headerIO = BytesIO(self._args['header'])
+                headerRDR = csv.reader(headerIO, delimiter=self._args['delimiter'], quotechar=self._args['quotechar'])
+                for row in headerRDR:
+                    header = row
+                    break
+
+                if 'hostname' not in header:
+                    printErrorMessage("Your header must contain 'hostname'")
+                    sys.exit(1)
+                hostname_field = header.index('hostname')
+            
+            if self._args['exclude'] != "":
+                excludeIO = BytesIO(self._args['exclude'])
+                excludeRDR = csv.reader(excludeIO, delimiter=self._args['delimiter'], quotechar=self._args['quotechar'])
+                for row in excludeRDR:
+                    exclude = list(set(row))
+                    break
+
+                nexclude = []
+                for el in exclude:
+                    if el != 'hostname' and el in header :
+                        nexclude.append(el)
+                exclude = nexclude[:]
+
+            res = dict()
+
+            valueIO = BytesIO(value)
+            valueRDR = csv.reader(valueIO, delimiter=self._args['delimiter'], quotechar=self._args['quotechar'])
+            for row in valueRDR:
+                if len(row) == len(header):
+                    thostvars = { row[hostname_field]: dict(zip(header, row)) }
+                    for el in exclude:
+                        del thostvars[row[hostname_field]][el]
+                    res.update( thostvars )
+            self._hostvars = res
+
+
         else:
             return {}
 
@@ -77,12 +152,14 @@ hostvars configuration:
 module             mandatory, name of the modules (curl in this case)
 description        optional, describe the datasource
 url                mandatory, URL to get 
-format             optional, format the result is in (defaults to json)
+format             optional, format the result is in. Currently supports json and csv (defaults to json)
+header             optional, a csv list of fields the url will yield. Only for csv
+exclude            optional, don't parse the fields matching this comma separated list of fieldnames
 
 groups configuration: (TBD)
 [curl-groups]    name it however you want...
 module             mandatory, name of the modules (curl in this case)
 enabled            optional, whether the config is enabled or not (defaults to enabled)
 url                mandatory, URL to get 
-format             optional, format the result is in (defaults to json)
+format             optional, format the result is in currently supports json and csv (defaults to json)
 """
